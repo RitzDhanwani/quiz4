1fe9ff113422e3f5e0f834a7ea36c3a34379181b
RitzDhanwani <ritzdhanwani@gmail.com>
2023-11-20 19:08:26 +0500

    quiz4

--- quiz4/src/App.js
+++ quiz4/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+import Navbar from "./components/Navbar";
+import Offerings from "./components/Offerings";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import Contactus from "./components/Contactus";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className="main">
+        <Navbar />
+        <Offerings />
+        <Contactus />
+        <Welcome />
+        <Footer />
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
