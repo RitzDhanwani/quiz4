7782b0c9beae28238c55dd73c1cb4517bf634cea
RitzDhanwani <ritzdhanwani@gmail.com>
2023-11-20 19:23:45 +0500

    quiz4

--- quiz4/src/App.js
+++ quiz4/src/App.js
@@ -16,6 +16,7 @@ const App = () => {
         <Contactus />
         <Welcome />
         <Footer />
+        {/*  */}
       </main>
     </div>
   )
--- quiz4/src/components/Navbar.js
+++ quiz4/src/components/Navbar.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CompanyLogo from '../assets/images/footer-logo.svg.png'
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <div className="navbar">
-      <div className="navbar-left">
-        <img src="/path/to/logo.png" alt="Logo" className="logo" />
-        <span className="logo-name">Your Company Name</span>
-      </div>
-      <div className="navbar-right">
-        <Link to="/product">Product</Link>
-        <Link to="/use-cases">Use cases</Link>
-        <Link to="/pricing">Pricing</Link>
-        <Link to="/customers">Customers</Link>
-        <Link to="/resources">Resources</Link>
-        <button className="signup-button">Sign Up</button>
-      </div>
-    </div>
+    <>
+    <Navbar expand="lg" style={{backgroundColor: '#FFF0EC'}}>
+      <Container fluid>
+        <Navbar.Brand href="#">
+            <img src={CompanyLogo} alt="logo" width={45} height={45}/><b> Writealy </b>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-right flex-grow-1 pe-3">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll>
+            <Nav.Link href="#action1"><b>Product - </b></Nav.Link>
+            <Nav.Link href="#action2"><b>Use cases - </b></Nav.Link>
+            <Nav.Link href="#action3"><b>Pricing</b></Nav.Link>
+            <Nav.Link href="#action3"><b>Customer</b></Nav.Link>
+            <Nav.Link href="#action3"><b>Resources - </b></Nav.Link>
+          </Nav>
+            <Button style={{backgroundColor: '#FFF0EC', color:'black', borderRadius: '30px', outlineColor: 'black'}}>Sign in</Button>
+         </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
